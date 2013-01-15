658656b343ca6732e9a62ad5d4190dbef77f0cc2
Richard van der Hoff <git@rvanderhoff.org.uk>
2013-01-15 22:43:20 +0000

    add some files, and a script to populate the local directory

---
+++ mklinks.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+mydir=$(dirname "$(readlink -f "$0")")
+
+link()
+{
+    target="$mydir/$1"
+    link=$2
+
+    if [ ! -e "$link" ]; then 
+        ln -vs "$target" "$link"
+        return
+    fi
+    
+    currenttarget=`readlink -f "$link"`
+    if [ "$currenttarget" = "$target" ]; then
+        echo "$link -> $target already in place"
+        return
+    fi
+
+    echo "$link exists and is not a link to $target" >&2
+    exit 1
+}
+
+link "bashrc" ~/.bashrc
+link "profile" ~/.profile
+link "gitconfig" ~/.gitconfig
