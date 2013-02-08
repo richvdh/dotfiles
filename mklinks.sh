9c7872ca37e43861c77b0cc2af90c91c6d20c690
Richard van der Hoff <git@rvanderhoff.org.uk>
2013-02-08 18:18:04 +0000

    support for gitconfig

--- mklinks.sh
+++ mklinks.sh
@@ -2,6 +2,7 @@
 
 mydir=$(dirname "$(readlink -f "$0")")
 
+
 link()
 {
     target="$mydir/$1"
@@ -22,7 +23,36 @@ link()
     exit 1
 }
 
+copy()
+{
+    target="$mydir/$1"
+    link=$2
+
+    if [ ! -e "$link" ]; then 
+        cp -v "$target" "$link"
+        return
+    fi
+    
+    if cmp -s $link $target; then
+        echo "$link == $target already in place"
+        return
+    fi
+
+    echo "$link exists and is not a copy of $target" >&2
+    exit 1
+}
+
 link "bashrc" ~/.bashrc
 link "profile" ~/.profile
-link "gitconfig" ~/.gitconfig
-link "emacs" ~/.emacs
+
+case `uname -o` in
+    Cygwin)
+        # can't use a copy here
+        copy "gitconfig.cygwin" ~/.gitconfig
+        ;;
+    *)
+        link "gitconfig" ~/.gitconfig
+        ;;
+esac
+
+# link "emacs" ~/.emacs
